a894a01a4feb381f2fa16d0da7ca95767f596417
nita-create <nitachroun168@gmail.com>
2026-07-08 16:59:44 +0700

    Dashboard Layout #114

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import StudentPickerView from '@/views/StudentPickerView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'student-picker',
+      component: StudentPickerView,
+    },
+  ],
 })
 
 export default router
